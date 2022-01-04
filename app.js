fe71bab8e42f423856c413b2a1ef261ca1a1c158
Carl-RobertMots <72864477+Carl-RobertMots@users.noreply.github.com>
2022-01-04 15:00:26 +0200

    Populated data structure items to list

--- app.js
+++ app.js
@@ -14,19 +14,44 @@ const ItemCtrl = (function(){
     }
 
     return {
+        getItems: function (){
+            return data.items
+        }
+    }
+    return {
         logData: function (){
             return data
         }
     }
 })();
 
+//UI CTRL
 const UICtrl = (function (){
+    return {
+        populateItemList: function(items){
+            let html = "";
+
+            items.forEach(function (item){
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary content">
+                    <i class="edit-item fa fa-pencil"></i>
+                </a>
+                </li>`;
+            });
+            //insert list items
+            document.querySelector("#item-list").innerHTML = html;
+        }
+    }
 })();
 
 const App = (function(ItemCtrl, UICtrl){
     return {
         init: function(){
             console.log("Initializing App")
+            //Getting items from data structure
+            const items =ItemCtrl.getItems()
+            UICtrl.populateItemList(items)
         }
     }
 })(ItemCtrl, UICtrl);
